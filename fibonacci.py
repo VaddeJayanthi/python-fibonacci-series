97391df0391f093680b050009bba071de5884d06
VaddeJayanthi <vjayanti689@gmail.com>
2026-09-21 11:25:36 +0530

    Add Fibonacci series implementation in fibonacci.py

---
+++ fibonacci.py
@@ -0,0 +1,14 @@
+n = int(input("Enter number of terms: "))
+
+a = 0
+b = 1
+
+print("Fibonacci Series:")
+
+for i in range(n):
+    print(a, end=" ")
+    a, b = b, a + b
+OUTPUT:
+Enter number of terms: 7
+Fibonacci Series:
+0 1 1 2 3 5 8 
